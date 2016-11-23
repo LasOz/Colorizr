b8030981eee357f2e0e0548c553bd32cb83fb978
Laser Ozdonmez <lo1368@my.bristol.ac.uk>
2016-11-23 21:57:02 +0000

    Layed out a break down of how to handle the palette reduction.

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@
 
 void extra_filters(cv::Mat &image, char const * folder);
 
-int get_levels(cv::Mat &input);
-
-void palette_reduce(cv::Mat &input, cv::Mat &output, cv::Mat &palette, int max_colours);
-
 int main(int argc, char *argv[])
 {
     tinyfd_messageBox("Before we begin...", "You should see a success message, if not then something went badly wrong...", "ok", "info", 1);
@@ -43,8 +39,8 @@ int main(int argc, char *argv[])
     else
         tinyfd_messageBox("Bad file name!", "Could not read image, due to incorrect file name", "ok", "error", 1);
 
-    im_color = cv::imread(file_open, cv::IMREAD_COLOR);
-    extra_filters(im_color, folder);
+    //im_color = cv::imread(file_open, cv::IMREAD_COLOR);
+    //extra_filters(im_color, folder);
 
     //Forgive this concatenation through conversion. Getting undefined behaviour with strcat
     std::string s1("Pictures saved successfully to:\n");
@@ -58,30 +54,15 @@ void extra_filters(cv::Mat & image, char const * folder)
 {
     cv::Mat output, palette_mat;
     cv::resize(image, image, cv::Size(256, 240), 0, 0, cv::INTER_NEAREST);
-    std::cout << get_levels(image) << std::endl;
+
+    // Change colours to nearest NES Counter parts (Patlette Reduce), Pixel by pixel using NN assign.
+    // Return the palette that is used.
+
+    // Sort the palette from most used to least used (requires making a structure to hold a colour histogram)
+
+    // Count colours, if >25 (of some other number) reduce by using only the 25 most used colours from previous palette
+
+    // Go into each 8x8 space and reduce colours to 4
+
     //palette_reduce(image, output, palette_mat, 25);
-}
-
-int get_levels(cv::Mat & input)
-{
-    int colors = 0;
-    std::unordered_map<cv::Vec3b, bool> table;
-    for (int j = 0; j < input.rows; ++j)
-        for (int i = 0; i < input.cols; ++i)
-        {
-            if (table[input.at<cv::Vec3b>(j, i)])
-            {
-                table.insert(std::pair<cv::Vec3b,bool>(input.at<cv::Vec3b>(j, i),true));
-                colors++;
-            }
-        }
-    return colors;
-}
-
-void palette_reduce(cv::Mat & input, cv::Mat & output, cv::Mat & palette, int max_colours)
-{
-    //Count colours in original image
-    //If greater than max group similar colours together. (k-mediods? / Heirachical with complete linkage?)
-    //Use the mediods to change the colour of the image to the nearest mediod.
-    //Replace each colour with its nearest neighbour to the palette
 }
